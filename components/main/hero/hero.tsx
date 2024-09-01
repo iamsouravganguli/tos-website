c7fb71a3df265a050cce2e785075e6259a6a7221
Anuj9675 <anujupadhyay6265@gmail.com>
2024-09-01 18:11:45 +0530

    align hero text with logo

--- components/main/hero/hero.tsx
+++ components/main/hero/hero.tsx
@@ -2,22 +2,29 @@
 
 import React from 'react';
 import { heroData } from '@/data/index';
+import Link from 'next/link';
 
 export function Hero() {
   const { title, description, buttonText1, buttonLink1, buttonText2, buttonLink2, image } = heroData;
 
   return (
     <section className="flex flex-col md:flex-row items-center justify-between h-screen bg-sky-50 text-black px-6 md:px-12 pt-16 w-full">
-      <div className="flex flex-col justify-center max-w-xl space-y-4 md:space-y-6 mx-4 md:mx-20 mt-6 mb-6 md:mt-0 md:mb-0">
+      <div className="flex flex-col justify-center max-w-xl space-y-4 md:space-y-6  md:mx-20 mt-6 mb-6 md:mt-0 md:mb-0">
         <h1 className="text-3xl md:text-6xl font-bold leading-tight">{title}</h1>
         <p className="text-sm md:text-base lg:text-lg">{description}</p>
-        <div className="flex flex-col md:flex-row gap-4 md:gap-6">
-          <a href={buttonLink1} className="w-fit bg-blue-600 text-white px-4 py-2 md:px-6 md:py-3 rounded-full text-sm md:text-base shadow-lg hover:bg-blue-700 transition-all duration-300">
+        <div className="flex flex-col md:flex-row gap-2 md:gap-6">
+          <Link
+            href={buttonLink1} 
+            className="md:w-48 md:h-12 w-32 h-10 bg-blue-600 text-white flex items-center justify-center rounded-full text-base shadow-lg hover:bg-blue-700 transition-all duration-300"
+          >
             {buttonText1}
-          </a>
-          <a href={buttonLink2} className="w-fit bg-transparent border-2 border-blue-700 px-4 py-2 md:px-6 md:py-3 rounded-full text-sm md:text-base hover:bg-blue-700 hover:text-white text-blue-600 transition-all duration-300">
+          </Link>
+          <Link 
+            href={buttonLink2} 
+            className="md:w-48 md:h-12 w-32 h-10 bg-transparent border-2 border-blue-700 flex items-center justify-center rounded-full text-base hover:bg-blue-700 hover:text-white text-blue-600 transition-all duration-300"
+          >
             {buttonText2}
-          </a>
+          </Link>
         </div>
       </div>
       <div className="flex items-center justify-center flex-1 px-6 md:px-0">
--- data/heroData.ts
+++ data/heroData.ts
@@ -6,8 +6,8 @@ export const heroData: HeroData = {
   title: 'Empowering Your Business with Cutting-Edge IT Solutions',
   description: 'At Totally Optimized Solutions, we deliver innovative technology services that drive growth and efficiency.',
   buttonText1: 'Our Services',
-  buttonLink1: '/services',
+  buttonLink1: '#services',
   buttonText2: 'Contact Us',
-  buttonLink2: '/contact',
+  buttonLink2: '#contact-us',
   image: '/assets/hero.png', 
 }
