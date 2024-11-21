0ea7b6014038c7be76bc10f743e575eeb75501ff
Mansi1527 <mansiy1527@icloud.com>
2024-11-21 12:40:47 +0530

    upts

--- data/heroData.ts
+++ data/heroData.ts
@@ -5,7 +5,7 @@ import { HeroData } from '@/types';
 export const heroData: HeroData = {
   // title: 'Empowering Your Business with Cutting-Edge IT Solutions',
   title:"Shaping ideas into products",
-  description: 'At Totally Optimized Solutions, we deliver innovative technology services that drive growth and efficiency.',
+  description: 'At Totally Optimised Solutions, we deliver innovative technology services that drive growth and efficiency.',
   buttonText1: 'Our Services',
   buttonLink1: '#services',
   buttonText2: 'Contact Us',
