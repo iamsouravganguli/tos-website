19dddcb2afc3d19aa3e25f8388e5e563accbe773
Mansi1527 <mansiy1527@icloud.com>
2024-12-10 12:33:34 +0530

    experience

--- components/experience/index.tsx
+++ components/experience/index.tsx
@@ -9,28 +9,28 @@ export const Experience = () => {
             <div className="text-center flex flex-col items-center">
                 <Image src="/assets/experience/experience.png" alt="" width={150} height={200} />
                 <h1 className="text-2xl md:text-3xl lg:text-4xl mt-2 font-semibold">
-                    <CountUp start={1} end={12} duration={2} />+
+                    <CountUp start={1} end={12} duration={totalDuration}/>+
                 </h1>
                 <p className="text-sm md:text-base text-gray-500 mt-2">Years of Experience</p>
             </div>
             <div className="text-center flex flex-col items-center">
                 <Image src="/assets/experience/projects.png" alt="" width={100} height={200} />
                 <h1 className="text-2xl md:text-3xl lg:text-4xl mt-2 font-semibold">
-                    <CountUp start={1} end={350} duration={3} />+
+                    <CountUp start={1} end={350} duration={totalDuration}/>+
                 </h1>
                 <p className="text-sm md:text-base text-gray-500 mt-2">Project Portfolio</p>
             </div>
             <div className="text-center flex flex-col items-center">
                 <Image src="/assets/experience/brand.png" alt="" width={100} height={200} />
                 <h1 className="text-2xl md:text-3xl lg:text-4xl mt-2 font-semibold">
-                    <CountUp start={1} end={155} duration={3} />+
+                    <CountUp start={1} end={155} duration={totalDuration}/>+
                 </h1>
                 <p className="text-sm md:text-base text-gray-500 mt-2">Branding Creations</p>
             </div>
             <div className="text-center flex flex-col items-center">
                 <Image src="/assets/experience/digital.png" alt="" width={150} height={200} />
                 <h1 className="text-2xl md:text-3xl lg:text-4xl mt-2 font-semibold">
-                    <CountUp start={1} end={125} duration={2.5} />+
+                    <CountUp start={1} end={125} duration={totalDuration} />+
                 </h1>
                 <p className="text-sm md:text-base text-gray-500 mt-2">Digital Assets</p>
             </div>
