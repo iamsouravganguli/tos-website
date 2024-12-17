08736bfd9f8aec8b26884a0d5edde71b4c28733f
Mansi1527 <mansiy1527@icloud.com>
2024-12-17 21:07:48 +0530

    updates member of team

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import { Timer } from '@/components/timer'
+
 import { Services } from '@/components/serices'
 const page = () => {
   return (
     <div>
       <Services/>
-      <Timer endDate="2024-12-13T00:00:00" />
+    
     </div>
   )
 }
